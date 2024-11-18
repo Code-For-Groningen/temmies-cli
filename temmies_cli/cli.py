a638abbc5c94e3a451b3439a9f88e2c1e3d7eb6f
Boyan <boyan@confest.im>
2024-11-18 20:23:55 +0100

    Implemented support for full course extraction

--- temmies_cli/cli.py
+++ temmies_cli/cli.py
@@ -3,18 +3,26 @@ from .commands.init import init_assignment
 from .commands.submit import submit_file
 from .commands.status import status_overview
 
+
 @click.group()
 def cli():
     """Temmies CLI - A command line tool for managing assignments using the Temmies library."""
     pass
 
+
 @cli.command()
-@click.argument('year_course_assignment', required=False)
+@click.argument('year_course_path', required=False)
 @click.argument('path', required=False, default='.')
 @click.option('-s', '--search', help='Search for an assignment by name.')
-def init(year_course_assignment, path, search):
-    """Initialize a new assignment."""
-    init_assignment(year_course_assignment, path, search)
+@click.option('-t', '--test-folder', default='.', help='Specify the name of the test cases folder.')
+def init(year_course_path, path, search, test_folder):
+    """
+    Initialize a new assignment or folder.
+
+    YEAR_COURSE_PATH: Format '{startyear-endyear}/{courseTag}' or '{startyear-endyear}/{courseTag}/{folder_or_assignment}'.
+    """
+    init_assignment(year_course_path, path, search, test_folder)
+
 
 @cli.command()
 @click.argument('files', nargs=-1, required=True)
@@ -23,11 +31,13 @@ def submit(files, quiet):
     """Submit file(s) to the relevant assignment."""
     submit_file(files, quiet)
 
+
 @cli.command()
 @click.option('-d', '--detail', is_flag=True, help="Add more detail to the status overview.")
 def status(detail):
     """Show the current assignment's status."""
     status_overview(detail)
+
 
 if __name__ == '__main__':
     cli()
